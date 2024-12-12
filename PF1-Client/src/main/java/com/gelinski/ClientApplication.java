3ea7353913b9d56f25febf3937d2be135c46743f
Gelinski <gus_gelinski@hotmail.com>
2024-12-12 12:34:45 -0300

    last commit

--- PF1-Client/src/main/java/com/gelinski/ClientApplication.java
+++ PF1-Client/src/main/java/com/gelinski/ClientApplication.java
@@ -1,14 +1,11 @@
 package com.gelinski;
 
-import com.gelinski.dto.request.CreateAccountRequest;
-import com.gelinski.dto.request.LoginRequest;
-import com.gelinski.dto.request.LogoutRequest;
 import com.google.gson.Gson;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class ClientApplication {
     private static final Gson gson = new Gson();
@@ -17,7 +14,7 @@ public class ClientApplication {
     private BufferedReader in;
     private BufferedReader stdIn;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ClientServerConnector connector = new ClientServerConnector();
 
         connector.connect();
--- PF1-Server/src/main/java/com/gelinski/ServerApplication.java
+++ PF1-Server/src/main/java/com/gelinski/ServerApplication.java
@@ -8,9 +8,7 @@ import java.sql.SQLException;
 
 @RequiredArgsConstructor
 public class ServerApplication {
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException {
         ServerSocketCommunicationService.startServer();
-
-//        new Thread(() -> new Server(22001)).start();
     }
 }
